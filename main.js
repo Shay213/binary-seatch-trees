180fe02f5569f15363adba196f21e48f30168981
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-03-21 14:20:36 +0100

    Create bst from array and print it

---
+++ main.js
@@ -0,0 +1,42 @@
+class BinarySearchTree{
+    constructor(array){
+        this.array = array.sort((a,b) => a-b).filter((el, i, arr) => arr.indexOf(el) === i);
+        this.root = this.buildTree();
+    }
+
+    buildTree = (start = 0, end = this.array.length-1) => {
+        if(start > end) return null;
+        const mid = parseInt((start + end) / 2);
+        const node = new Node(this.array[mid]);
+        node.left = this.buildTree(start, mid-1);
+        node.right = this.buildTree(mid+1, end);
+        return node;
+    };
+}
+
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null) {
+       return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+}
+
+const bst = new BinarySearchTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+prettyPrint(bst.root);
+
+console.log(bst.insert(2));
+prettyPrint(bst.root);
